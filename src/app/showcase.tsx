9a294c52db25298abd47e4201b82fb800910f454
Christian Assante <assantech@hivesoft.it>
2025-10-02 11:34:40 +0200

    Choose

--- src/app/showcase.tsx
+++ src/app/showcase.tsx
@@ -9,22 +9,27 @@ export default function Showcase() {
   ];
 
   return (
-    <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-gray-900 sm:mt-5 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-      {showcase.map((site) => (
-        <div
-          key={site.id}
-          className="flex flex-col gap-y-3 border-l border-gray-900/15 pl-6"
-        >
-          <dt className="text-sm/6">
-            <a href={site.url} target="_blank" className="hover:underline">
-              {site.name}
-            </a>
-          </dt>
-          {/* <dd className="order-first text-3xl font-semibold tracking-tight">
+    <>
+      <p className="text-blue-400 font-bold pb-8 lg:pb-6 text-center md:text-left fade-in">
+        You can choose between:
+      </p>
+      <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-gray-900 sm:mt-5 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        {showcase.map((site) => (
+          <div
+            key={site.id}
+            className="flex flex-col gap-y-3 border-l border-gray-900/15 pl-6"
+          >
+            <dt className="text-sm/6">
+              <a href={site.url} target="_blank" className="hover:underline">
+                {site.name}
+              </a>
+            </dt>
+            {/* <dd className="order-first text-3xl font-semibold tracking-tight">
             {site.name}
           </dd> */}
-        </div>
-      ))}
-    </dl>
+          </div>
+        ))}
+      </dl>
+    </>
   );
 }
